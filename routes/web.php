ec82a4004d20aee8f2ed37ae4525c956df861ae4
TemitopeGX <temitopeayomikun999@gmail.com>
2026-06-05 09:24:20 +0100

    Add clear cache route to fix CORS

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/clear', function () {
+    \Illuminate\Support\Facades\Artisan::call('optimize:clear');
+    return 'Cache cleared successfully!';
 });
